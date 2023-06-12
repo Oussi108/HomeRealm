e81035df8c33722db633c6ee49361cebdd989529
Oussi108 <live.houssin@gmail.com>
2023-06-12 02:55:39 +0100

    lastest Version V3

--- HomeRealm_Project/Controllers/SettingController.cs
+++ HomeRealm_Project/Controllers/SettingController.cs
@@ -12,21 +12,56 @@ namespace HomeRealm_Project.Controllers
         // GET: Setting
         public ActionResult Index()
         {
-            User CurrentUser;
+            SettingModel CurrentUser;
+            int id = int.Parse(Session["iduser"].ToString());
             using (MydbContext db = new MydbContext()) {
-                CurrentUser = db.Users.Find(Session["iduser"]);
-                if (CurrentUser is null) {
+                CurrentUser = new SettingModel() { user = db.Users.Find(Session["iduser"]), host = db.Hosts.Where(d => d.User.Id == id).FirstOrDefault() };
+                if (CurrentUser.user is null) {
                     return Redirect("Home");
                 }
             
             }
             return View(CurrentUser);
         }
-        public ActionResult ChangeUser()
+        [HttpPost]
+        
+        public ActionResult Submitchange(FormCollection formCollection)
         {
+            int id = int.Parse(Session["iduser"].ToString());
+            MydbContext db = new MydbContext();
+            // Retrieve the current user from the database
+            var currentUser = db.Users.FirstOrDefault(u => u.Id == id);
 
-            return View();
+            // Update the user attributes based on the form data
+            currentUser.FirstName = formCollection["Firstname"];
+            currentUser.LastName = formCollection["Lastname"];
+            currentUser.Email = formCollection["Email"];
+           
+
+            // Check if the password fields are not empty
+            if (!string.IsNullOrEmpty(formCollection["oldpass"]) && !string.IsNullOrEmpty(formCollection["newpass"]) && !string.IsNullOrEmpty(formCollection["newpassC"]))
+            {
+                string oldPassword = formCollection["oldpass"];
+                string newPassword = formCollection["newpass"];
+                string confirmPassword = formCollection["newpassC"];
+
+               if(oldPassword== currentUser.Password && newPassword== confirmPassword) { 
+                // Update the user's password
+                currentUser.Password = newPassword;
+            }
+            }
+
+            // Save the changes to the database
+            db.SaveChanges();
+
+            // Redirect to a success page or return a success message
+            return RedirectToAction("Index");
         }
+
+
+
+
+
 
     }
 }
---
+++ HomeRealm_Project/Models/SettingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeRealm_Project.Models
+{
+    public class SettingModel
+    {
+        public User user { get; set; }
+        public Host host { get; set; }
+    }
+}
